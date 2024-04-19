7d5bbcf1640f29d0aa1487e5cbeda41370ae4e2e
Keiran Price <keiran.price@stfc.ac.uk>
2024-04-19 09:00:44 +0100

    Create the base strategy

---
+++ plotting_service/strategies/base.py
@@ -0,0 +1,22 @@
+"""
+The Base Strategy to be implemented per instrument.
+"""
+
+from abc import ABC, abstractmethod
+from pathlib import Path
+
+from plotting_service.model import Plot
+
+
+class PlotStrategy(ABC):
+    """
+    Abstract Plot Strategy to be implemented per instrument, defining the plot building strategy.
+    """
+
+    @abstractmethod
+    def generate_plot_data(self, nexus_file: Path) -> Plot:
+        """
+        Generate the plot data for the given nexus file
+        :return:
+        """
+        pass
