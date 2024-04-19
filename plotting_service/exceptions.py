3c3942e19ba59b79fbdb33e75fabacd6539ceea0
Keiran Price <keiran.price@stfc.ac.uk>
2024-04-19 09:08:47 +0100

    Add custom exceptions

---
+++ plotting_service/exceptions.py
@@ -0,0 +1,11 @@
+"""
+Module containing application specific exceptions
+"""
+
+
+class MissingStrategyError(Exception):
+    """
+    Exception raised when a strategy is missing
+    """
+
+    pass
