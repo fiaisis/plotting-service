6ee1638d024d7ecc0df48a9a4ddbd775bf29d9ac
Keiran Price <keiran.price@stfc.ac.uk>
2024-05-01 11:41:45 +0100

    Switch to h5grove

--- plotting_service/plotting_api.py
+++ plotting_service/plotting_api.py
@@ -3,11 +3,13 @@ Main module
 """
 
 import logging
+import os
 import sys
+from typing import Literal
 
 from fastapi import FastAPI
-
-from plotting_service.router import ROUTER
+from h5grove.fastapi_utils import router, settings  # type: ignore
+from starlette.middleware.cors import CORSMiddleware
 
 stdout_handler = logging.StreamHandler(stream=sys.stdout)
 logging.basicConfig(
@@ -16,8 +18,32 @@ logging.basicConfig(
     level=logging.INFO,
 )
 logger = logging.getLogger(__name__)
+logger.info("Starting Plotting Service")
+
+ALLOWED_ORIGINS = ["*"]
+CEPH_DIR = os.environ.get("CEPH_DIR", "/ceph")
+logger.info("Setting ceph directory to %s", CEPH_DIR)
+settings.base_dir = os.path.abspath(CEPH_DIR)
 
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ALLOWED_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-app.include_router(ROUTER)
+
+@app.get("/healthz")
+async def get() -> Literal["ok"]:
+    """
+    Health check endpoint
+    \f
+    :return: "ok"
+    """
+    return "ok"
+
+
+app.include_router(router)
