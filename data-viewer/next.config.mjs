16442ecc2924b2b7c78ab03f972001850a789312
Keiran Price <keiran.price@stfc.ac.uk>
2024-05-02 13:02:24 +0100

    Add asset prefix

--- data-viewer/next.config.mjs
+++ data-viewer/next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "standalone",
-    basePath: "/data-viewer"
+    basePath: "/data-viewer",
+    assetPrefix: "/data-viewer"
 };
 
 export default nextConfig;
