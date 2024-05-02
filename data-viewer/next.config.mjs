840cc8514c03b04314dd4cb3cb444de0a64b5a37
Keiran Price <keiran.price@stfc.ac.uk>
2024-05-02 13:06:37 +0100

    Reset config

--- data-viewer/next.config.mjs
+++ data-viewer/next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "standalone",
-    basePath: "/data-viewer",
-    assetPrefix: "/data-viewer"
+    output: "standalone"
 };
 
 export default nextConfig;
