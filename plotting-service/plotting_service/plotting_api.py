608acd19cb8fd10b2dac3216af7c5a505cda29ae
Keiran Price <keiran.price@stfc.ac.uk>
2024-06-27 13:37:20 +0100

    Integrate auth

--- plotting-service/plotting_service/plotting_api.py
+++ plotting-service/plotting_service/plotting_api.py
@@ -11,6 +11,10 @@ from typing import Literal
 from fastapi import FastAPI
 from h5grove.fastapi_utils import router, settings  # type: ignore
 from starlette.middleware.cors import CORSMiddleware
+from starlette.requests import Request
+
+from plotting_service.auth import get_user_from_token, get_experiments_for_user
+from plotting_service.exceptions import AuthError
 
 stdout_handler = logging.StreamHandler(stream=sys.stdout)
 logging.basicConfig(
@@ -45,6 +49,32 @@ async def get() -> Literal["ok"]:
     :return: "ok"
     """
     return "ok"
+
+
+@app.middleware("http")
+async def check_permissions(request: Request, call_next: Callable[..., Any]) -> Any:
+    """
+    Middleware that checks the requestee token has permissions for that experiment
+    :param request: The request to check
+    :param call_next: The next call (the route function called)
+    :return: A response
+    """
+    experiment_number = re.search(r"%2FRB(\d+)%2F", request.url.query).group(1)
+    if request.method == "OPTIONS":
+        return await call_next(request)
+    token = request.headers.get("Authorization").split(" ")[1]
+    try:
+        user = get_user_from_token(token)
+    except AuthError:
+        raise HTTPException(403, detail="Forbidden")
+    if user.role == "staff":
+        return await call_next(request)
+
+    allowed_experiments = get_experiments_for_user(user)
+    if experiment_number in allowed_experiments:
+        return await call_next(request)
+
+    raise HTTPException(403, detail="Forbidden")
 
 
 app.include_router(router)
