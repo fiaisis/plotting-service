8046a4d040eb1f2375d1ff991d80d1b2c34db728
Keiran Price <keiran.price@stfc.ac.uk>
2024-07-10 10:50:56 +0100

    Add explicit check

--- plotting-service/plotting_service/plotting_api.py
+++ plotting-service/plotting_service/plotting_api.py
@@ -7,6 +7,7 @@ import os
 import re
 import sys
 import typing
+from http import HTTPStatus
 from pathlib import Path
 
 from fastapi import FastAPI, HTTPException
@@ -54,72 +55,78 @@ async def get() -> typing.Literal["ok"]:
     return "ok"
 
 
+def is_safe_path(path: Path) -> bool:
+    base_dir = Path(CEPH_DIR).resolve()
+    user_path = path.resolve()
+
+    return base_dir in user_path.parents
+
+
 @app.get("/text/instrument/{instrument}/experiment_number/{experiment_number}", response_class=PlainTextResponse)
 async def get_text_file(instrument: str, experiment_number: int, filename: str) -> str:
-    if ("/" in instrument or "\\" in instrument) or ("/" in filename or "\\" in filename):
-        logger.warning("Possible directory traversal attack")
-        raise HTTPException(status_code=400)
-
     path = (
         Path(CEPH_DIR)
         .joinpath(instrument.upper(), "RBNumber", f"RB{experiment_number}", "autoreduced", filename)
         .relative_to(CEPH_DIR)
     )
 
+    if not is_safe_path(path):
+        raise HTTPException(status_code=HTTPStatus.FORBIDDEN)
+
     with path.open("r") as file:
         return file.read()
-
-
-@app.middleware("http")
-async def check_permissions(request: Request, call_next: typing.Callable[..., typing.Any]) -> typing.Any:
-    """
-    Middleware that checks the requestee token has permissions for that experiment
-    :param request: The request to check
-    :param call_next: The next call (the route function called)
-    :return: A response
-    """
-    if DEV_MODE:
-        return await call_next(request)
-    if request.method == "OPTIONS":
-        return await call_next(request)
-    if request.url.path in ("/healthz", "/docs"):
-        return await call_next(request)
-
-    logger.info(f"Checking permissions for {request.url.path}")
-
-    if request.url.path.startswith("/text"):
-        experiment_number = request.url.path.split("/")[-1]
-    else:
-        match = re.search(r"%2FRB(\d+)%2F", request.url.query)
-        if match is not None:
-            experiment_number = int(match.group(1))
-        else:
-            logger.warning(
-                f"The requested nexus metadata path {request.url.path} does not include an experiment number. "
-                f"Permissions cannot be checked"
-            )
-            raise HTTPException(400, "Request missing experiment number")
-
-    auth_header = request.headers.get("Authorization")
-    if auth_header is None:
-        raise HTTPException(401, "Unauthenticated")
-
-    token = auth_header.split(" ")[1]
-
-    try:
-        user = get_user_from_token(token)
-    except AuthError:
-        raise HTTPException(403, detail="Forbidden") from None
-    logger.info("Checking role of user")
-    if user.role == "staff":
-        return await call_next(request)
-
-    logger.info("Checking experiments for user")
-    allowed_experiments = get_experiments_for_user(user)
-    if experiment_number in allowed_experiments:
-        return await call_next(request)
-
-    raise HTTPException(403, detail="Forbidden")
-
-
-app.include_router(router)
+#
+#
+# @app.middleware("http")
+# async def check_permissions(request: Request, call_next: typing.Callable[..., typing.Any]) -> typing.Any:
+#     """
+#     Middleware that checks the requestee token has permissions for that experiment
+#     :param request: The request to check
+#     :param call_next: The next call (the route function called)
+#     :return: A response
+#     """
+#     if DEV_MODE:
+#         return await call_next(request)
+#     if request.method == "OPTIONS":
+#         return await call_next(request)
+#     if request.url.path in ("/healthz", "/docs"):
+#         return await call_next(request)
+#
+#     logger.info(f"Checking permissions for {request.url.path}")
+#
+#     if request.url.path.startswith("/text"):
+#         experiment_number = request.url.path.split("/")[-1]
+#     else:
+#         match = re.search(r"%2FRB(\d+)%2F", request.url.query)
+#         if match is not None:
+#             experiment_number = int(match.group(1))
+#         else:
+#             logger.warning(
+#                 f"The requested nexus metadata path {request.url.path} does not include an experiment number. "
+#                 f"Permissions cannot be checked"
+#             )
+#             raise HTTPException(400, "Request missing experiment number")
+#
+#     auth_header = request.headers.get("Authorization")
+#     if auth_header is None:
+#         raise HTTPException(401, "Unauthenticated")
+#
+#     token = auth_header.split(" ")[1]
+#
+#     try:
+#         user = get_user_from_token(token)
+#     except AuthError:
+#         raise HTTPException(403, detail="Forbidden") from None
+#     logger.info("Checking role of user")
+#     if user.role == "staff":
+#         return await call_next(request)
+#
+#     logger.info("Checking experiments for user")
+#     allowed_experiments = get_experiments_for_user(user)
+#     if experiment_number in allowed_experiments:
+#         return await call_next(request)
+#
+#     raise HTTPException(403, detail="Forbidden")
+#
+#
+# app.include_router(router)
