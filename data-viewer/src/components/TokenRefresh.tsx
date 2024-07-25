1d13360cdf34307284229246ec34ebbfc149173a
Keiran Price <keiran.price@stfc.ac.uk>
2024-07-25 09:02:32 +0100

    Automatically refresh

--- data-viewer/src/components/TokenRefresh.tsx
+++ data-viewer/src/components/TokenRefresh.tsx
@@ -1,19 +1,38 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const TokenRefresh = () => {
+  const [refreshCount, setRefreshCount] = useState<number>(0);
   useEffect(() => {
-    const token = localStorage.getItem("scigateway:token");
-    fetch("/auth/api/jwt/refresh", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ token: token }),
-    })
-      .then((response) => response.json())
-      .then((data) => localStorage.setItem("scigateway:token", data.token))
-      .catch((_) => console.error("Could not refresh token"));
+    const refreshToken = () => {
+      setRefreshCount(refreshCount + 1);
+      const token = localStorage.getItem("scigateway:token");
+      fetch("/auth/api/jwt/refresh", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ token: token }),
+      })
+        .then((response) => response.json())
+        .then((data) => localStorage.setItem("scigateway:token", data.token))
+        .catch((_) => console.error("Could not refresh token"));
+    };
+
+    refreshToken();
+    // Set up the interval to refresh the token every 10 minutes (600000 milliseconds)
+    const intervalId = setInterval(refreshToken, 600000);
+
+    // Set up the timeout to clear the interval after 12 hours (43200000 milliseconds)
+    const timeoutId = setTimeout(() => {
+      clearInterval(intervalId);
+    }, 43200000);
+
+    // Clear the interval and timeout when the component unmounts
+    return () => {
+      clearInterval(intervalId);
+      clearTimeout(timeoutId);
+    };
   }, []);
 
   return <></>;
