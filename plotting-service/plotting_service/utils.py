09eec325e5e1f975e2918c962d2d465961e5b936
Samuel Jones <samuel.jones@stfc.ac.uk>
2024-10-18 09:49:55 +0100

    Do small refactor

--- plotting-service/plotting_service/utils.py
+++ plotting-service/plotting_service/utils.py
@@ -42,37 +42,30 @@ def find_file(ceph_dir: str, instrument: str, experiment_number: int, filename: 
 
 
 def find_experiment_number(request: Request) -> int:
-    experiment_number = None
     if request.url.path.startswith("/text"):
-        experiment_number = int(request.url.path.split("/")[-1])
-    elif request.url.path.startswith("/find_file"):
+        return int(request.url.path.split("/")[-1])
+    if request.url.path.startswith("/find_file"):
         url_parts = request.url.path.split("/")
-        last_part_seen = url_parts[-1]
-        for part in reversed(url_parts):
-            if "experiment_number" in part:
-                experiment_number = int(last_part_seen)
-            else:
-                last_part_seen = part
-        if experiment_number is None:
-            # Avoiding circular import
+        try:
+            experiment_number_index = url_parts.index("experiment_number")
+            return int(url_parts[experiment_number_index + 1])
+        except (ValueError, IndexError):
             from plotting_service.plotting_api import logger
 
             logger.warning(
                 f"The requested path {request.url.path} does not include an experiment number. "
                 f"Permissions cannot be checked"
             )
-            raise HTTPException(400, "Request missing experiment number")
+            raise HTTPException(400, "Request missing experiment number") from None
     else:
         match = re.search(r"%2FRB(\d+)%2F", request.url.query)
         if match is not None:
-            experiment_number = int(match.group(1))
-        else:
-            # Avoiding circular import
-            from plotting_service.plotting_api import logger
+            return int(match.group(1))
+        # Avoiding circular import
+        from plotting_service.plotting_api import logger
 
-            logger.warning(
-                f"The requested nexus metadata path {request.url.path} does not include an experiment number. "
-                f"Permissions cannot be checked"
-            )
-            raise HTTPException(400, "Request missing experiment number")
-    return experiment_number
+        logger.warning(
+            f"The requested nexus metadata path {request.url.path} does not include an experiment number. "
+            f"Permissions cannot be checked"
+        )
+        raise HTTPException(400, "Request missing experiment number")
