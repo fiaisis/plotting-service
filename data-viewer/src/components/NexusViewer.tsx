f7e2c10a89a78d362dad1c4888c89bd4dc1db28d
Samuel Jones <samuel.jones@stfc.ac.uk>
2024-10-22 11:10:59 +0100

    Better handle finding files, and cleanup some issues. Removes race cond.

--- data-viewer/src/components/NexusViewer.tsx
+++ data-viewer/src/components/NexusViewer.tsx
@@ -1,9 +1,10 @@
 "use client";
 import "@h5web/app/dist/styles.css";
 import { App, H5GroveProvider } from "@h5web/app";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { ErrorBoundary } from "react-error-boundary";
-import { Box, CircularProgress } from '@mui/material';
+import { Stack, CircularProgress } from '@mui/material';
+import Image from "next/image";
 
 const Fallback = () => (
   <div
@@ -15,7 +16,7 @@ const Fallback = () => (
       height: "100vh",
     }}
   >
-    <img src={"/monkey.webp"} alt={"Monkey holding excellent website award"} />
+    <Image src={"/monkey.webp"} alt={"Monkey holding excellent website award"} />
     <h1>Something Went Wrong</h1>
     <p>
       Return <a href={"https://reduce.isis.cclrc.ac.uk"}>Home</a>
@@ -35,60 +36,59 @@ export default function NexusViewer(props: {
 }) {
   // We need to turn the env var into a full url as the h5provider can not take just the route.
   // Typically, we expect API_URL env var to be /plottingapi in staging and production
-  const [hostName, setHostName] = useState<string>("");
-  const [protocol, setProtocol] = useState<string>("http");
   const [filepath, setFilePath] = useState<string>("");
+  const [token, setToken] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(true);
-  const groveApiUrl =
-    props.apiUrl === "http://localhost:8000"
-      ? props.apiUrl
-      : `${protocol}//${hostName}/plottingapi`;
-  const fileQueryUrl = `${props.apiUrl}/find_file/instrument/${props.instrument}/experiment_number/${props.experimentNumber}`
-  const fileQueryParams = `filename=${props.filename}`;
+  const [groveApiUrl, setApiUrl] = useState<string>(props.apiUrl)
 
-  useEffect(() => {
-    setLoading(true);
-    setHostName(window.location.hostname);
-    setProtocol(window.location.protocol);
+  useEffect (() => {
+    setLoading(true)
+    const loadedToken = localStorage.getItem("scigateway:token") ?? ""
+    setToken(loadedToken);
+    setApiUrl(props.apiUrl.includes("localhost") ? props.apiUrl : `${window.location.protocol}//${window.location.hostname}/plottingapi`)
+
+    const fileQueryUrl = `${props.apiUrl}/find_file/instrument/${props.instrument}/experiment_number/${props.experimentNumber}`
+    const fileQueryParams = `filename=${props.filename}`;
     const headers: { [key: string]: string } = {'Content-Type': 'application/json'};
-    if (token != "") {
-      headers['Authorization'] = `Bearer ${token}`;
+    console.log("Token set as: " + loadedToken)
+    if (loadedToken != "") {
+      headers['Authorization'] = `Bearer ${loadedToken}`;
     }
+
     fetch(`${fileQueryUrl}?${fileQueryParams}`, {method: 'GET', headers})
       .then((res) => {
-            if (!res.ok) {
-              throw new Error(res.statusText);
-            }
-            return res.text();
+          if (!res.ok) {
+            throw new Error(res.statusText);
+          }
+          return res.text();
       })
       .then((data) => {
-          const filepath_to_use = data.replace(/"/g, "")
+          const filepath_to_use = data.split("%20").join(" ").replace(/"/g, "")
           setFilePath(filepath_to_use);
+          setLoading(false)
       })
-  }, [])
+    }, [props.apiUrl, props.instrument, props.experimentNumber, props.filename])
+
   return (
     <ErrorBoundary FallbackComponent={Fallback}>
-        { loading ? (
-          <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
-            <CircularProgress />
-          </Box>
-        ) : (
-          <H5GroveProvider
-          url={groveApiUrl}
-          filepath={filepath.split("%20").join(" ")}
-          axiosConfig={useMemo(
-            () => ({
-              params: { file: filepath.split("%20").join(" ").replace(/"/g, "") },
-              headers: {
-                Authorization: `Bearer ${token}`,
-              },
-            }),
-            [filepath],
-          )}
-          >
-            <App propagateErrors />
-          </H5GroveProvider>
-        )}
+      { loading ? (
+        <Stack spacing={2} sx={{justifyContent: 'center', alignItems: 'center', height: '100%', width: '100%'}}>
+         <p>Finding your file</p>
+         <CircularProgress/>
+        </Stack>
+      ) : (
+      <H5GroveProvider
+        url={groveApiUrl}
+        filepath={filepath}
+        axiosConfig={{
+          params: { file: filepath },
+          headers: {
+            Authorization: `Bearer ${token}`,
+          }
+        }}
+      >
+        <App propagateErrors />
+      </H5GroveProvider> )}
     </ErrorBoundary>
   );
 }
