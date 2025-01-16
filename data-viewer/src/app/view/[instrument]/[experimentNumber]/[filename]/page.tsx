9dea8f41962ff4d9b3dba1c11e7c0aa0ae92fcb4
Samuel Jones <samuel.jones@stfc.ac.uk>
2025-01-16 16:29:39 +0000

    Add support for csv for textfiles

--- data-viewer/src/app/view/[instrument]/[experimentNumber]/[filename]/page.tsx
+++ data-viewer/src/app/view/[instrument]/[experimentNumber]/[filename]/page.tsx
@@ -7,15 +7,16 @@ export default function DataPage({
                                  }: {
     params: { instrument: string; experimentNumber: string; filename: string };
 }) {
-// We expect a route of /instrument_name/experiment_number/filename
-// This will result in a slug list of [instrument_name, experiment_number, filename]
+    // We expect a route of /instrument_name/experiment_number/filename
+    // This will result in a slug list of [instrument_name, experiment_number, filename]
     const {instrument, experimentNumber, filename} = params;
     const fileExtension = filename.split(".").pop();
     const apiUrl = process.env.API_URL ?? "http://localhost:8000";
-
+    // Files to be viewed with the TextViewer, as text files
+    const textFiles: Array<string> = ["txt", "csv"];
     return (
         <main className="h5-container">
-            {fileExtension === "txt" ? (
+            {textFiles.includes(fileExtension) ? (
                 <TextViewer
                     apiUrl={apiUrl}
                     experimentNumber={experimentNumber}
