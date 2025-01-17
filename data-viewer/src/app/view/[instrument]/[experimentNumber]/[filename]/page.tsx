35967c082be2a212146c1f771a3b98a890c3922b
Samuel Jones <samjones714@gmail.com>
2025-01-17 10:55:37 +0000

    Update data-viewer/src/app/view/[instrument]/[experimentNumber]/[filename]/page.tsx
    Co-authored-by: keiranjprice101 <44777678+keiranjprice101@users.noreply.github.com>

--- data-viewer/src/app/view/[instrument]/[experimentNumber]/[filename]/page.tsx
+++ data-viewer/src/app/view/[instrument]/[experimentNumber]/[filename]/page.tsx
@@ -10,10 +10,7 @@ export default function DataPage({
     // We expect a route of /instrument_name/experiment_number/filename
     // This will result in a slug list of [instrument_name, experiment_number, filename]
     const {instrument, experimentNumber, filename} = params;
-    let fileExtension = filename.split(".").pop();
-    if (fileExtension == undefined) {
-        fileExtension = "";
-    }
+const fileExtension = filename.split(".").pop() ?? ""
     const apiUrl = process.env.API_URL ?? "http://localhost:8000";
     // Files to be viewed with the TextViewer, as text files
     const textFiles: Array<string> = ["txt", "csv"];
