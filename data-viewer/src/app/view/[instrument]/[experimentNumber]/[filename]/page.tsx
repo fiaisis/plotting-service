c4fc38eb225a39d5250832f1c9a18d9cc4e5359d
Samuel Jones <samuel.jones@stfc.ac.uk>
2025-01-17 10:40:10 +0000

    Add undefined fileExtension handling

--- data-viewer/src/app/view/[instrument]/[experimentNumber]/[filename]/page.tsx
+++ data-viewer/src/app/view/[instrument]/[experimentNumber]/[filename]/page.tsx
@@ -10,7 +10,10 @@ export default function DataPage({
     // We expect a route of /instrument_name/experiment_number/filename
     // This will result in a slug list of [instrument_name, experiment_number, filename]
     const {instrument, experimentNumber, filename} = params;
-    const fileExtension = filename.split(".").pop();
+    let fileExtension = filename.split(".").pop();
+    if (fileExtension == undefined) {
+        fileExtension = "";
+    }
     const apiUrl = process.env.API_URL ?? "http://localhost:8000";
     // Files to be viewed with the TextViewer, as text files
     const textFiles: Array<string> = ["txt", "csv"];
