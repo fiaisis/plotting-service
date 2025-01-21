7b965d3eb8c8ae36c3f09a8e038c1a227e488cd7
keiranjprice101 <44777678+keiranjprice101@users.noreply.github.com>
2025-01-21 15:38:34 +0000

    Fix commas in file names (#223)
    * Replace url encoded comma with actual comma in grove request
    
    * Temp build on push
    
    * Revert temp build-push

--- data-viewer/src/app/view/[instrument]/[experimentNumber]/[filename]/page.tsx
+++ data-viewer/src/app/view/[instrument]/[experimentNumber]/[filename]/page.tsx
@@ -3,34 +3,34 @@ import "../../../../globals.css";
 import TextViewer from "@/components/TextViewer";
 
 export default function DataPage({
-                                     params,
-                                 }: {
-    params: { instrument: string; experimentNumber: string; filename: string };
+  params,
+}: {
+  params: { instrument: string; experimentNumber: string; filename: string };
 }) {
-    // We expect a route of /instrument_name/experiment_number/filename
-    // This will result in a slug list of [instrument_name, experiment_number, filename]
-    const {instrument, experimentNumber, filename} = params;
-const fileExtension = filename.split(".").pop() ?? ""
-    const apiUrl = process.env.API_URL ?? "http://localhost:8000";
-    // Files to be viewed with the TextViewer, as text files
-    const textFiles: Array<string> = ["txt", "csv"];
-    return (
-        <main className="h5-container">
-            {textFiles.includes(fileExtension) ? (
-                <TextViewer
-                    apiUrl={apiUrl}
-                    experimentNumber={experimentNumber}
-                    instrument={instrument}
-                    filename={filename}
-                />
-            ) : (
-                <NexusViewer
-                    apiUrl={apiUrl}
-                    experimentNumber={experimentNumber}
-                    instrument={instrument}
-                    filename={filename}
-                />
-            )}
-        </main>
-    );
-};
+  // We expect a route of /instrument_name/experiment_number/filename
+  // This will result in a slug list of [instrument_name, experiment_number, filename]
+  const { instrument, experimentNumber, filename } = params;
+  const fileExtension = filename.split(".").pop() ?? "";
+  const apiUrl = process.env.API_URL ?? "http://localhost:8000";
+  // Files to be viewed with the TextViewer, as text files
+  const textFiles: Array<string> = ["txt", "csv"];
+  return (
+    <main className="h5-container">
+      {textFiles.includes(fileExtension) ? (
+        <TextViewer
+          apiUrl={apiUrl}
+          experimentNumber={experimentNumber}
+          instrument={instrument}
+          filename={filename}
+        />
+      ) : (
+        <NexusViewer
+          apiUrl={apiUrl}
+          experimentNumber={experimentNumber}
+          instrument={instrument}
+          filename={filename}
+        />
+      )}
+    </main>
+  );
+}
--- data-viewer/src/components/NexusViewer.tsx
+++ data-viewer/src/components/NexusViewer.tsx
@@ -1,78 +1,106 @@
 "use client";
 import "@h5web/app/dist/styles.css";
-import {App, H5GroveProvider} from "@h5web/app";
-import {useEffect, useState} from "react";
-import {ErrorBoundary} from "react-error-boundary";
-import {CircularProgress, Stack} from '@mui/material';
-import {FileQueryUrl} from "@/components/utils/FileQueryUrl";
-import {Fallback} from "@/components/utils/FallbackPage";
+import { App, H5GroveProvider } from "@h5web/app";
+import { useEffect, useState } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { CircularProgress, Stack } from "@mui/material";
+import { FileQueryUrl } from "@/components/utils/FileQueryUrl";
+import { Fallback } from "@/components/utils/FallbackPage";
 
-export default function NexusViewer(props :{
-    filename: string;
-    apiUrl: string;
-    instrument?: string;
-    experimentNumber?: string;
-    userNumber?: string;
-}
-) {
-    // We need to turn the env var into a full url as the h5provider can not take just the route.
-    // Typically, we expect API_URL env var to be /plottingapi in staging and production
-    const [filepath, setFilePath] = useState<string>("");
-    const [token, setToken] = useState<string>("");
-    const [loading, setLoading] = useState<boolean>(true);
-    const [groveApiUrl, setApiUrl] = useState<string>(props.apiUrl)
+export default function NexusViewer(props: {
+  filename: string;
+  apiUrl: string;
+  instrument?: string;
+  experimentNumber?: string;
+  userNumber?: string;
+}) {
+  // We need to turn the env var into a full url as the h5provider can not take just the route.
+  // Typically, we expect API_URL env var to be /plottingapi in staging and production
+  const [filepath, setFilePath] = useState<string>("");
+  const [token, setToken] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(true);
+  const [groveApiUrl, setApiUrl] = useState<string>(props.apiUrl);
 
-    useEffect(() => {
-        setLoading(true)
-        const loadedToken = localStorage.getItem("scigateway:token") ?? ""
-        setToken(loadedToken);
-        setApiUrl(props.apiUrl.includes("localhost") ? props.apiUrl : `${window.location.protocol}//${window.location.hostname}/plottingapi`)
-
-        const fileQueryUrl = FileQueryUrl(props.apiUrl, props.instrument, props.experimentNumber, props.userNumber);
-        if (fileQueryUrl == null) {
-            throw new Error("The API file query URL was not rendered correctly and returned null")
-        }
-
-        const fileQueryParams = `filename=${props.filename}`;
-        const headers: { [key: string]: string } = {'Content-Type': 'application/json'};
-        if (loadedToken != "") {
-            headers['Authorization'] = `Bearer ${loadedToken}`;
-        }
-
-        fetch(`${fileQueryUrl}?${fileQueryParams}`, {method: 'GET', headers})
-            .then((res) => {
-                if (!res.ok) {
-                    throw new Error(res.statusText);
-                }
-                return res.text();
-            })
-            .then((data) => {
-                const filepath_to_use = data.split("%20").join(" ").replace(/"/g, "")
-                setFilePath(filepath_to_use);
-                setLoading(false)
-            })
-    }, [props.apiUrl, props.instrument, props.experimentNumber, props.userNumber, props.filename])
-
-    return (
-        <ErrorBoundary FallbackComponent={Fallback}>
-            {loading ? (
-                <Stack spacing={2} sx={{justifyContent: 'center', alignItems: 'center', height: '100%', width: '100%'}}>
-                    <p>Finding your file</p>
-                    <CircularProgress/>
-                </Stack>
-            ) : (
-                <H5GroveProvider
-                    url={groveApiUrl}
-                    filepath={filepath}
-                    axiosConfig={{
-                        params: {file: filepath},
-                        headers: {
-                            Authorization: `Bearer ${token}`,
-                        }
-                    }}
-                >
-                    <App propagateErrors/>
-                </H5GroveProvider>)}
-        </ErrorBoundary>
+  useEffect(() => {
+    setLoading(true);
+    const loadedToken = localStorage.getItem("scigateway:token") ?? "";
+    setToken(loadedToken);
+    setApiUrl(
+      props.apiUrl.includes("localhost")
+        ? props.apiUrl
+        : `${window.location.protocol}//${window.location.hostname}/plottingapi`,
     );
+
+    const fileQueryUrl = FileQueryUrl(
+      props.apiUrl,
+      props.instrument,
+      props.experimentNumber,
+      props.userNumber,
+    );
+    if (fileQueryUrl == null) {
+      throw new Error(
+        "The API file query URL was not rendered correctly and returned null",
+      );
+    }
+
+    const fileQueryParams = `filename=${props.filename}`;
+    const headers: { [key: string]: string } = {
+      "Content-Type": "application/json",
+    };
+    if (loadedToken != "") {
+      headers["Authorization"] = `Bearer ${loadedToken}`;
+    }
+
+    fetch(`${fileQueryUrl}?${fileQueryParams}`, { method: "GET", headers })
+      .then((res) => res.text())
+      .then((data) => {
+        const filepath_to_use = data
+          .split("%20")
+          .join(" ")
+          .split("%C")
+          .join(",")
+          .replace(/"/g, "");
+        setFilePath(filepath_to_use);
+      })
+      .catch((error) => Error(error))
+      .finally(() => setLoading(false));
+  }, [
+    props.apiUrl,
+    props.instrument,
+    props.experimentNumber,
+    props.userNumber,
+    props.filename,
+  ]);
+
+  return (
+    <ErrorBoundary FallbackComponent={Fallback}>
+      {loading ? (
+        <Stack
+          spacing={2}
+          sx={{
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100%",
+            width: "100%",
+          }}
+        >
+          <p>Finding your file</p>
+          <CircularProgress />
+        </Stack>
+      ) : (
+        <H5GroveProvider
+          url={groveApiUrl}
+          filepath={filepath}
+          axiosConfig={{
+            params: { file: filepath },
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }}
+        >
+          <App propagateErrors />
+        </H5GroveProvider>
+      )}
+    </ErrorBoundary>
+  );
 }
