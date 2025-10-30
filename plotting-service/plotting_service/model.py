b4c8deacfc226c2736c1bf800e7ffec0a7085058
Vladimir Garanin <57632637+vovsike@users.noreply.github.com>
2025-10-30 09:20:10 +0000

    Fix not getting metadata for MARI type files (#286)
    * Fix not getting metadata for MARI type files
    
    * Formatting and linting commit
    
    * Fix import for Metadata
    
    * Formatting and linting commit
    
    * Fix types for Metadata
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- plotting-service/plotting_service/model.py
+++ plotting-service/plotting_service/model.py
@@ -1,12 +1,13 @@
+from typing import Any
+
 from pydantic import BaseModel
 
 
 class Metadata(BaseModel):
     filename: str
     shape: int
-    x_axis_label: str
+    axes_labels: dict[str, Any]
     x_axis_min: float
     x_axis_max: float
-    y_axis_label: str
     y_axis_min: float
     y_axis_max: float
