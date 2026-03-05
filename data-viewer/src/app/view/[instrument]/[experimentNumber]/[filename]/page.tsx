79369cc121194c03b696c4c950eb5a27ad58da11
Keiran Price <keiran.price@stfc.ac.uk>
2026-03-05 09:33:47 +0000

    Make `DataPage` async and refactor `params` destructuring logic for cleaner type handling.

--- data-viewer/src/app/view/[instrument]/[experimentNumber]/[filename]/page.tsx
+++ data-viewer/src/app/view/[instrument]/[experimentNumber]/[filename]/page.tsx
@@ -2,41 +2,41 @@ import NexusViewer from "@/components/NexusViewer";
 import "../../../../globals.css";
 import TextViewer from "@/components/TextViewer";
 
-// 1. Make the component async
 export default async function DataPage({
-                                           params,
-                                       }: {
-    // 2. Wrap the params type in a Promise
-    params: Promise<{ instrument: string; experimentNumber: string; filename: string }>;
+  params,
+}: {
+  params: Promise<{
+    instrument: string;
+    experimentNumber: string;
+    filename: string;
+  }>;
 }) {
-    // We expect a route of /instrument_name/experiment_number/filename
-    // This will result in a slug list of [instrument_name, experiment_number, filename]
+  // We expect a route of /instrument_name/experiment_number/filename
+  // This will result in a slug list of [instrument_name, experiment_number, filename]
 
-    // 3. Await the params before destructuring
-    const { instrument, experimentNumber, filename } = await params;
+  const { instrument, experimentNumber, filename } = await params;
 
-    const fileExtension = filename.split(".").pop() ?? "";
-    const apiUrl = process.env.API_URL ?? "http://localhost:8000";
-    // Files to be viewed with the TextViewer, as text files
-    const textFiles: Array<string> = ["txt", "csv", "gss", "abc", "prm"];
+  const fileExtension = filename.split(".").pop() ?? "";
+  const apiUrl = process.env.API_URL ?? "http://localhost:8000";
+  const textFiles: Array<string> = ["txt", "csv", "gss", "abc", "prm"];
 
-    return (
-        <main className="h5-container">
-            {textFiles.includes(fileExtension) ? (
-                <TextViewer
-                    apiUrl={apiUrl}
-                    experimentNumber={experimentNumber}
-                    instrument={instrument}
-                    filename={filename}
-                />
-            ) : (
-                <NexusViewer
-                    apiUrl={apiUrl}
-                    experimentNumber={experimentNumber}
-                    instrument={instrument}
-                    filename={filename}
-                />
-            )}
-        </main>
-    );
+  return (
+    <main className="h5-container">
+      {textFiles.includes(fileExtension) ? (
+        <TextViewer
+          apiUrl={apiUrl}
+          experimentNumber={experimentNumber}
+          instrument={instrument}
+          filename={filename}
+        />
+      ) : (
+        <NexusViewer
+          apiUrl={apiUrl}
+          experimentNumber={experimentNumber}
+          instrument={instrument}
+          filename={filename}
+        />
+      )}
+    </main>
+  );
 }
